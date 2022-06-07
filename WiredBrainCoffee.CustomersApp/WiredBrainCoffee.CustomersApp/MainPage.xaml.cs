9c0269cbd7c9bac745fa2cd23e8ea6bf32b59397
Bronson Brown <BrBrown@rideuta.com>
2022-06-07 13:30:00 -0600

    Updates.

---
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using System;
+using Windows.UI.Popups;
+using WiredBrainCoffee.CustomersApp.DataProvider;
+using System.Linq;
+using WiredBrainCoffee.CustomersApp.Model;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WiredBrainCoffee.CustomersApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private CustomerDataProvider _customerDataProvider;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
+        }
+        private void Current_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            customerListView.Items.Clear();
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+        private async void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(
+            customerListView.Items.OfType<Customer>());
+            deferral.Complete();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
+        }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var customer = (Customer)customerListView.SelectedItem;
+            if(customer != null)
+            {
+                customerListView.Items.Remove(customer);
+            }
+        }
+
+        private void ButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+            //Returns an object so we need to cast integer
+            int column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+
+
+            /*if(column == 0) column = 2 else column = 0 0 */
+            int newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(customerListGrid, newColumn);
+
+            moveSymbol.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+
+        }
+
+        private void customerListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = (Customer)customerListView.SelectedItem;
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            chkIsDeveloper.IsChecked = customer?.isDeveloper;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = (Customer)customerListView.SelectedItem;
+            if(customer != null)
+            {
+                customer.FirstName = txtFirstName.Text;
+                customer.LastName = txtLastName.Text;
+                customer.isDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+            }
+        }
+    }
+}
